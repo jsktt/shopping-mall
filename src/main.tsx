f0f93fd8cc53eb42a66b1ff441cd3b74538333c2
leastzero <csong7177@naver.com>
2026-01-14 17:31:00 +0900

    fix: index.css import 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ProductProvider } from './contexts/ProductContext';
 import { CartProvider } from './contexts/CartContext';
